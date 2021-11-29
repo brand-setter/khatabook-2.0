bb62dc9f0a9ce6b21852f0fc4377e9d680c8ba3d
Dhanush <dhanushpowerk6@gmail.com>
2021-11-29 09:55:56 +0530

    Splash Screen Added and modified some other files

---
+++ app/src/main/java/com/example/khatabook_20/Splash_bg.kt
@@ -0,0 +1,16 @@
+package com.example.khatabook_20
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Splash_bg : AppCompatActivity() {
+    @SuppressLint("ResourceType")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val intent = Intent(this, GoogleSignIn::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
