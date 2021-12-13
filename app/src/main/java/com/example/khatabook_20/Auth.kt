8f66c0d1a0e4ae59dd1e8938bdf37d1fc7633e4b
reshma <reshmaroyal2002@gmail.com>
2021-12-13 11:03:49 +0530

    enabled databinding

--- app/src/main/java/com/example/khatabook_20/Auth.kt
+++ app/src/main/java/com/example/khatabook_20/Auth.kt
@@ -3,19 +3,20 @@ package com.example.khatabook_20
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
+import com.example.khatabook_20.databinding.ActivityAuthBinding
 import com.google.android.material.tabs.TabLayout
 
 class Auth:AppCompatActivity() {
 
-
+        private lateinit var binding: ActivityAuthBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
 
         supportActionBar!!.hide()
 
-       val tablayout = findViewById<TabLayout>(R.id.tablayout)
-        val viewpager = findViewById<ViewPager>(R.id.viewpager)
+       val tablayout = binding.tablayout
+        val viewpager = binding.viewpager
         tablayout.addTab(tablayout.newTab().setText("SignIn"))
         tablayout.addTab(tablayout.newTab().setText("SignUp"))
         tablayout.tabGravity = TabLayout.GRAVITY_FILL
